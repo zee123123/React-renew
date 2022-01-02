28c8f4ffa761f56668c3452e492f44291be37520
zee123123 <louisliu000123@gmail.com>
2022-01-02 11:50:41 -0330

    Express Initialize

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const http = require("http");
+//const http = require("http");
+const express = require("express");
+const app = express();
 
 let notes = [
   {
@@ -20,9 +22,20 @@ let notes = [
     important: true,
   },
 ];
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(notes));
+// const app = http.createServer((request, response) => {
+//   response.writeHead(200, { "Content-Type": "application/json" });
+//   response.end(JSON.stringify(notes));
+// });
+// const app = http.createServer((request, response) => {
+//   response.writeHead(200, { "Content-Type": "application/json" });
+//   response.end(JSON.stringify(notes));
+// });
+app.get('/', (request, response)=>{
+    response.send("<h1>Hello Server</h1>");
+})
+
+app.get("/notes", (request, response) => {
+response.json(notes);
 });
 
 const PORT = 3001;
